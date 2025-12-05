6fedbc708a530d69c239d7847ad4dd214b449037
Viwu <sinconsistencia@gmail.com>
2025-12-04 21:50:01 -0600

    player class controls var

---
+++ source/satl/Player.hx
@@ -0,0 +1,21 @@
+package satl;
+
+import flixel.input.keyboard.FlxKey;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite
+{
+    public var controls = {
+        left: [FlxKey.A, FlxKey.LEFT],
+        down: [FlxKey.S, FlxKey.DOWN],
+        up: [FlxKey.W, FlxKey.UP],
+        right: [FlxKey.D, FlxKey.RIGHT],
+
+        interact: [FlxKey.SPACE]
+    };
+
+	override public function new()
+	{
+		super();
+	}
+}
