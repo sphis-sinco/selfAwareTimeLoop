20129ef823db8d094d8bb9fda9e5e1cc6c818fd1
Viwu <sinconsistencia@gmail.com>
2025-12-04 21:57:01 -0600

    player in game

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
+import satl.Player;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var player:Player;
+
 	override public function create()
 	{
 		super.create();
+		
+		player = new Player();
+		add(player);
 	}
 
 	override public function update(elapsed:Float)
