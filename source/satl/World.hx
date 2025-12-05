349e6107c0e76b07387e3f180f0adf5de7d1037c
Viwu <sinconsistencia@gmail.com>
2025-12-05 07:14:18 -0600

    fix collisions system and fix tilemap shitz

--- source/satl/World.hx
+++ source/satl/World.hx
@@ -1,13 +1,16 @@
 package satl;
 
+import flixel.util.FlxDirectionFlags;
 import lime.utils.Assets;
 import haxe.Json;
 import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import flixel.tile.FlxTilemap;
 
+using StringTools;
+
 class World extends FlxTilemap
 {
-    public var map:FlxOgmo3Loader;
+	public var map:FlxOgmo3Loader;
 
 	override public function new(level_name:String)
 	{
@@ -15,12 +18,30 @@ class World extends FlxTilemap
 
 		map = new FlxOgmo3Loader('assets/data/levels/' + level_name + '.ogmo', 'assets/data/levels/' + level_name + '.json');
 		@:privateAccess
-		map.loadTilemap('assets/' + map.project.tilesets[0].path.split('..')[0], 'walls', this);
+		map.loadTilemap('assets/' + map.project.tilesets[0].path.replace('../', ''), 'walls', this);
 		follow();
 
 		for (tile in Json.parse(Assets.getText('assets/data/tileset.json'))?.tiles ?? [])
 		{
-			setTileProperties(tile.id, tile?.collision);
+			var collision:FlxDirectionFlags = switch (Std.string(tile?.collision).toLowerCase())
+			{
+				case 'left': LEFT;
+				case 'down': DOWN;
+				case 'up': UP;
+				case 'right': RIGHT;
+
+				case 'ceiling': CEILING;
+				case 'floor': FLOOR;
+				case 'wall': WALL;
+				case 'any': ANY;
+
+				case 'none': NONE;
+
+				default: NONE;
+			};
+
+			if (tile?.id != null)
+				setTileProperties(tile.id, collision);
 		}
 	}
 }
