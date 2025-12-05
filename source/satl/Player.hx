fdc7fd069da96b1986a704a5e931e87dfc8fbb00
Viwu <sinconsistencia@gmail.com>
2025-12-04 21:51:34 -0600

    max speed capping

--- source/satl/Player.hx
+++ source/satl/Player.hx
@@ -6,22 +6,31 @@ import flixel.FlxSprite;
 
 class Player extends FlxSprite
 {
-    public var controls = {
-        left: [FlxKey.A, FlxKey.LEFT],
-        down: [FlxKey.S, FlxKey.DOWN],
-        up: [FlxKey.W, FlxKey.UP],
-        right: [FlxKey.D, FlxKey.RIGHT],
+	public var controls = {
+		left: [FlxKey.A, FlxKey.LEFT],
+		down: [FlxKey.S, FlxKey.DOWN],
+		up: [FlxKey.W, FlxKey.UP],
+		right: [FlxKey.D, FlxKey.RIGHT],
+		interact: [FlxKey.SPACE]
+	};
 
-        interact: [FlxKey.SPACE]
-    };
-
-    public var current_speed:Float = 0.0;
-    public var max_speed:Float = 4.0;
+	public var current_speed:Float = 0.0;
+	public var max_speed:Float = 4.0;
 
 	override public function new()
 	{
 		super();
 
-        makeGraphic(16, 16, FlxColor.BLUE);
+		makeGraphic(16, 16, FlxColor.BLUE);
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (current_speed > max_speed)
+		{
+			current_speed = max_speed;
+		}
 	}
 }
