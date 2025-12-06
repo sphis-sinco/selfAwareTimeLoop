c7281d4196a1c50304224232d732aa931a1f5d44
Viwu <sinconsistencia@gmail.com>
2025-12-05 19:33:01 -0600

    sound

--- source/satl/DialogueBox.hx
+++ source/satl/DialogueBox.hx
@@ -1,5 +1,6 @@
 package satl;
 
+import flixel.sound.FlxSound;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
@@ -9,19 +10,37 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 
 class DialogueBox extends FlxTypedSpriteGroup<FlxSprite>
 {
-    public var box:FlxSprite;
-    public var text:FlxText;
+	public var box:FlxSprite;
+	public var text:FlxText;
+
+	public var voice_line:FlxSound;
 
 	override public function new(dialogue:String)
 	{
 		super();
 
-        box = new FlxSprite();
-        add(box);
+		box = new FlxSprite();
+		add(box);
 
-        box.makeGraphic(Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), FlxColor.WHITE);
+		box.makeGraphic(Std.int(FlxG.width / 2), Std.int(FlxG.height / 4), FlxColor.WHITE);
 
-        text = new FlxText(0, 0, box.width, dialogue);
-        add(text);
+		text = new FlxText(0, 0, box.width, dialogue);
+		text.color = FlxColor.BLACK;
+		add(text);
+
+		voice_line = new FlxSound();
+	}
+
+	public function setDialogue(dialogue:String, ?dialogue_sound:String = null)
+	{
+		text.text = dialogue;
+
+        voice_line.stop();
+
+		if (dialogue_sound != null)
+		{
+			voice_line.loadStream('assets/sounds/dialogue/' + dialogue_sound + '.wav');
+            voice_line.play();
+		}
 	}
 }
